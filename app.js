cb5c84181bf93070b3faea6e43bdbe2b2074a2e1
Anusha <anushasunil71201@gmail.com>
2021-09-11 04:15:16 +0530

    docs: added references of element tags

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 var inputText = document.querySelector("#inputBox")
+var btnTranslate = document.querySelector("#btn-translate");
+var outputText = document.querySelector("#outputBox")
+
+btnTranslate.addEventListener("click", function click () {
+    console.log(inputText.value)
+    
+})
