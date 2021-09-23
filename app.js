d75821148c3ba7532398de5a3da905e0a0ad3fc0
Anusha <anushasunil71201@gmail.com>
2021-09-23 10:19:21 +0530

    refactor: code format

--- app.js
+++ app.js
@@ -3,23 +3,23 @@ var btnTranslate = document.querySelector("#btn-translate");
 var outputText = document.querySelector("#outputBox")
 
 
-function errorHandling(error)
-{
+function errorHandling(error) {
     console.log("error occurred", error)
     alert("server error")
 }
 
-function fetchInput(inputValue)
-{
+function fetchInput(inputValue) {
     url = "https://api.funtranslations.com/translate/shakespeare.json?text=" + inputValue.value
     fetch(url)
-    .then(response => response.json())
-    .then(function outputTranslated(json){outputText.innerText = json.contents.translated})
-    .catch(errorHandling)
+        .then(response => response.json())
+        .then(function outputTranslated(json) {
+            outputText.innerText = json.contents.translated
+        })
+        .catch(errorHandling)
 }
 
 
-btnTranslate.addEventListener("click", function click () {
+btnTranslate.addEventListener("click", function click() {
     console.log(inputText.value)
     fetchInput(inputText)
 })
